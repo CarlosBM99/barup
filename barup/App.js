e8b936f41cec9ad0251f92e5655b128fecaf20bc
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2018-04-27 22:58:34 +0200

    set up

---
+++ barup/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'native-base';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Icon name="ios-home"/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
