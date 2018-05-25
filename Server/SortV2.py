717ac59db892562714074ff734edf594e199370f
Eric <3ric9898@gmail.com>
2018-05-26 01:26:43 +0200

    Filters adapted to new format

--- Server/SortV2.py
+++ Server/SortV2.py
@@ -11,46 +11,64 @@ config = {
 
 class Bar:
 	'Holds the data for a bar as it appears in the database'
-	beer = 0
-	beerPrice = 0
+	Day_1_Evening = 0.0
+	Day_1_Late_Night = 0.0
+	Day_1_Midday = 0.0
+	Day_1_Morning = 0.0
+	atmosphere = 0
+	beerPrice = 0.0
 	billiards = 0
-	crowd = "temp"
+	coffeePrice = 0.0
 	darts = 0
 	football = 0
 	id = 0
-	location = "temp"
-	name = "temp"
+	latitude = 0.0
+	longitude = 0.0
 	rating = 0
+	sodaPrice = 0.0
    
 
-	def __init__(self,beer, beerPrice, billiards, crowd, darts, football, id, location,name, rating):
-		self.beer = beer
+	def __init__(self,Day_1_Evening, Day_1_Late_Night, Day_1_Midday, Day_1_Morning,
+				atmosphere, beerPrice, billiards, coffeePrice,
+				darts, football, id, latitude, longitude, rating, sodaPrice):
+				
+		self.Day_1_Evening = Day_1_Evening
+		self.Day_1_Late_Night = Day_1_Late_Night
+		self.Day_1_Middays = Day_1_Midday
+		self.Day_1_Morning = Day_1_Morning
+		self.atmosphere = atmosphere
 		self.beerPrice = beerPrice
 		self.billiards = billiards
-		self.crowd = crowd
+		self.coffeePrice = coffeePrice
 		self.darts = darts
 		self.football = football
 		self.id = id
-		self.location = location
-		self.name = name
+		self.latitude = latitude
+		self.longitude = longitude
 		self.rating = rating
+		self.sodaPrice = sodaPrice
       
    
-def binSort(str, dict, keys):
-	delList = []
-	for index in range(0,len(keys)):
-		if dict[keys[index]][str] == 0:
-			delList.append(index)
-		
-	keys2 = keys
-	#print(len(delList))
-		
+def badgeFilter(str, bars):
 
-	for dIndex in sorted(delList, reverse=True):		
-		del dict[keys2[dIndex]]
-		keys.remove(keys2[dIndex])
-	
-
+	for index in sorted(range(1,len(bars)),reverse=True):
+		if bars[index][str] == 0:
+			del bars[index]
+	#print(bars)
+			
+def styleFilter(str, bars):
+	if str == "youthful":
+		cmp = 2
+	elif str == "luxurious":
+		cmp	= 3
+	elif str == "sport":
+		cmp	= 4
+	elif str == "familiar":
+		cmp = 1
+		
+	for index in sorted(range(1,len(bars)),reverse=True):
+		if bars[index]["atmosphere"] != cmp:
+			del bars[index]
    
 firebase = pyrebase.initialize_app(config)
 
@@ -74,19 +92,31 @@ all_bar = db.child("bars").get().val()
 keys = list(all_bar)
 
 sortInfo = db.child("status_search").child("-LCiGxdHnFR4pMroxKwi").get().val()#child(id gotten from app)
-#print(all_bar)
+
+#print(all_bar[1]["id"])
 
 
 if sortInfo["it"]["badgets"]["billiards"] == 1:
-	binSort("darts",all_bar,keys)
+	badgeFilter("billiards",all_bar)
 if sortInfo["it"]["badgets"]["darts"] == 1:
-	binSort("darts",all_bar,keys)
+	badgeFilter("darts",all_bar)
 if sortInfo["it"]["badgets"]["table_football"] == 1:
-	binSort("football",all_bar,keys)
+	badgeFilter("football",all_bar)
+#crowdedness
+	
+if sortInfo["it"]["style"]["familiar"] == 1:
+	styleFilter("familiar",all_bar)
+elif sortInfo["it"]["style"]["youthful"] == 1:
+	styleFilter("youthful",all_bar)
+elif sortInfo["it"]["style"]["luxurious"] == 1:
+	styleFilter("luxurious",all_bar)
+elif sortInfo["it"]["style"]["sport"] == 1:
+	styleFilter("sport",all_bar)
+print(all_bar)
 
 #need to get key from input
-
-###SECTION 2:get the database in a list of objects for sorting###
+'''
+###SECTION 2:get the database in a list of objects for sorting### perhaps not needed
 
 
 #print(all_bar[keys[0]]["beer"])
@@ -104,9 +134,9 @@ for id in range(0, len(keys)):
 for test in bList:
 	print(test.beer)
 	
-
-###SECTION 3:sort with gradient criteria, mergesort in parallel ###	
-
+'''
+###SECTION 3:sort with gradient criteria, mergesort in parallel ###	prices, rating, crowdedness
+'''
 ###SECTION 4:upload to result and tick off the confirmation flag ###
 resDict = dict()
 
@@ -116,8 +146,8 @@ for index in range(0, len(keys)):
 							"darts": bList[index].darts,"football": bList[index].football,
 							"id": bList[index].id,"location": bList[index].location,
 							"name": bList[index].name,"rating": bList[index].rating}
-							
-db.child("results").child("-LCiGxdHnFR4pMroxKwi").set(resDict)#change later o key
+'''							
+db.child("results").child("-LCiGxdHnFR4pMroxKwi").set(all_bar)#change later to key
 
 time.sleep(10)
 
