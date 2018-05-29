890568e85da3963d1acf11b9f567ec758a87fe2c
Eric <3ric9898@gmail.com>
2018-05-29 04:01:26 +0200

    code cleaned up

--- Server/SortV3.py
+++ Server/SortV3.py
@@ -32,25 +32,7 @@ def Sort(list,criteria):
 	l = len(list)
     
 	print ('List length : ', l)
-
-	# create an unsorted list with random numbers 
-	#start_time = time.time()
-	#a = [ random.randint(0, n*100) for n in range(0, l) ]
-	#print ('Random list generated in ', time.time() - start_time)
-	# start timing the single-core procedure 
-	#start_time = time.time()
-	#single = merge_sort(a)
-	#single_core_time = time.time() - start_time
-	#a_sorted = a[:]
-	#a_sorted.sort()
-	''' comparison with Python list's "sort" method, validation of the algorithm 
-	(it has to work right, doesn't it??) '''
-	#print ('Verification of sorting algorithm', a_sorted == single)
-	#print ('Single Core: %4.6f sec'%( single_core_time ))
 	if cores > 1:
-		''' we collect the list element count and the time taken 
-		for each of the procedures in a file '''
-		#f = open('mergesort-'+str(cores)+'.dat', 'a')
 		print ('Starting %d-core process'%cores)
 		start_time = time.time()
 		# divide the list in "cores" parts
@@ -58,39 +40,34 @@ def Sort(list,criteria):
 		offset = 0
 		p = []
 		for n in range(0, cores):
-			''' we create a new Process object and assign the "merge_sort_multi" function to it,
-			using as input a sublist '''
+			#we create a process and call merge_sort_multi with the necessary parameters
 			if n < cores - 1:
 				proc = Process( target=merge_sort_multi, args=( list[n*step:(n+1)*step],criteria ) )
 			else:
-				# get the remaining elements in the list
+				# get rest of elements in the last process
 				proc = Process( target=merge_sort_multi, args=( list[n*step:],criteria ) )
 			p.append(proc)
-
-		''' http://docs.python.org/library/multiprocessing.html#multiprocessing.Process.start & 
-		http://docs.python.org/library/multiprocessing.html#multiprocessing.Process.join each Process '''    
+  
 		for proc in p:
 			proc.start()
-		''' Corrected! '''
+			
 		for proc in p:
 			proc.join()
 		print ('Performing final merge...')
 		start_time_final_merge = time.time()
 		p = []
-		''' For a core count greater than 2, we can use multiprocessing 
-		again to merge sublists in parallel '''
+		
 		if len(responses) > 2:
 			while len(responses) > 0:
-				''' we remove sublists from the "responses" list and pass it as input to the 
-				"merge_multi" wrapper function of "merge" '''
+				# merge the sublists that come from each process
 				proc = Process( target=merge_multi, args=(responses.pop(0),responses.pop(0), criteria) )
 				p.append( proc )
-			# again starting and joining ( this seems like a pattern, doesn't it ... ? )
+			
 			for proc in p:
 				proc.start()
 			for proc in p:
 				proc.join()
-		# finally we have 2 sublists which need to be merged 
+		# final sublists 
 		a = merge(responses[0], responses[1], criteria)
 		
 		final_merge_time = time.time() - start_time_final_merge
@@ -99,12 +76,12 @@ def Sort(list,criteria):
 		print ('%d-Core ended: %4.6f sec'%(cores, multi_core_time))
 		return a
 		
-# a wrapper function which appends the result of "merge_sort" to the "responses" list
+
 def merge_sort_multi( list_part, criteria ):
 	
 	responses.append( merge_sort( list_part,criteria ) )
 
-# a wrapper function which appends the result of "merge" to the "responses" list
+
 def merge_multi( list_part_left, list_part_right, criteria ):
   responses.append( merge(list_part_left, list_part_right, criteria ) )
 
@@ -119,7 +96,7 @@ def merge_sort(a, criteria):
   a_right = merge_sort(a_right, criteria)
   return merge(a_left, a_right, criteria)
 
-# ... also explained earlier
+  
 def merge(left, right, criteria):
   a = []
   while len(left) > 0 or len(right) > 0:
@@ -153,14 +130,21 @@ db = firebase.database()
 manager = Manager() 
 responses = manager.list()
 
-all_bar = db.child("bars").get().val()
-keys = list(all_bar)
+if len(sys.argv) > 1: 
+	key = sys.argv[1]
+else:
+	key = "-LDTUfnsyoRn2-532OBG"
 
-sortInfo = db.child("status_search").child(sys.argv[1]).get().val()#child(id gotten from app)
+
+all_bar = db.child("bars").get().val()
+
+
+sortInfo = db.child("status_search").child(key).get().val()#child(id gotten from app)
 if all_bar[0] == None:
 	del all_bar[0]
 #print(all_bar[1]["id"])
 
+#filters commented for now, vut in working order
 '''
 if sortInfo["it"]["badgets"]["billards"] == 1:
 	badgeFilter("billards",all_bar)
@@ -174,57 +158,33 @@ if sortInfo["it"]["atmosphere"] != 0:
 	styleFilter(sortInfo["it"]["atmosphere"],all_bar)
 print(all_bar)
 
-#need to get key from input
-''''''
-###SECTION 2:get the database in a list of objects for sorting### perhaps not needed
 
-
-#print(all_bar[keys[0]]["beer"])
-
-bList = []
-
-for id in range(0, len(keys)):
-    
-	bList.append(Bar(all_bar[keys[id]]["beer"],all_bar[keys[id]]["beerPrice"],
-					all_bar[keys[id]]["billards"],all_bar[keys[id]]["crowd"],
-					all_bar[keys[id]]["darts"],all_bar[keys[id]]["football"],
-					all_bar[keys[id]]["id"],all_bar[keys[id]]["location"],
-					all_bar[keys[id]]["name"],all_bar[keys[id]]["rating"],))
-	
-for test in bList:
-	print(test.beer)
-	
 '''
 
-###SECTION 3:sort with gradient criteria, mergesort in parallel ###	prices, rating, crowdedness
-temp = list(all_bar[5:10])
-print(len(temp))
+###SECTION 2:sort with gradient criteria, mergesort in parallel ###	prices, rating, crowdedness
 
-all_bar = Sort(all_bar, "beerPrice")sys.argv[1]
+#elif for now since multiple sorting isn't implemented yet
 
-print(len(temp))
+if sortInfo["it"]["order"]["beer_prace"] == 1:
+	all_bar = Sort(all_bar, "beerPrice")
+elif sortInfo["it"]["order"]["crowdness"] == 1:
+	all_bar = Sort(all_bar, "prediction")
+elif sortInfo["it"]["order"]["rating"] == 1:
+	all_bar = Sort(all_bar, "rating")
+
+
+
 for index in all_bar:
 	print(index["beerPrice"])
 	
 
 
+###SECTION 3:upload to result and tick off the confirmation flag ###
+					
+#db.child("results").child(key).set(all_bar)#change later to key
 
-'''instantiate a multiprocessing.Manager object to store the output of each process, 
-see example here http://docs.python.org/library/multiprocessing.html#sharing-state-between-processes '''
+#db.child("status_search").child(key).update({"state": 0})
 
-'''
-###SECTION 4:upload to result and tick off the confirmation flag ###
-resDict = dict()
+#time.sleep(30)
 
-for index in range(0, len(keys)):
-	resDict[keys[index]] = {"beer": bList[index].beer,"beerPrice": bList[index].beerPrice,
-							"billards": bList[index].billards,"crowd": bList[index].crowd,
-							"darts": bList[index].darts,"football": bList[index].football,
-							"id": bList[index].id,"location": bList[index].location,
-							"name": bList[index].name,"rating": bList[index].rating}
-'''							
-db.child("results").child(sys.argv[1]).set(all_bar)#change later to key
-
-time.sleep(30)
-
-db.child("results").child(sys.argv[1]).remove()#change later to key
+#db.child("results").child(key).remove()#change later to key
